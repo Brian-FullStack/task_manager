43acedab76e26b90946968ee2596374a56ef4b41
Brian-FullStack <brian.fullstack@gmail.com>
2022-01-05 21:29:06 +0000

    Added functionality to login form

--- app.py
+++ app.py
@@ -31,8 +31,8 @@ def get_tasks():
 def register():
     if request.method == "POST":
         # check if username already exists in the db
-        existing_user = mongo.db.users.find(
-            {"username": request.form.get("username").lower()})
+        existing_user = mongo.db.users.find_one(
+           {"username": request.form.get("username").lower()})
 
         # If username exists then flash a message
         if existing_user:
@@ -51,6 +51,31 @@ def register():
         flash("Registration Successful!")
 
     return render_template("register.html")
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        # Check if username exists in the database
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            # If user exists, check password matches hashed password
+            # Display a message
+            if check_password_hash(
+                existing_user["password"], request.form.get("password")):
+                    session["user"] = request.form.get("username").lower()
+                    flash("Welcome {}, ".format(request.form.get("username")))
+            else:
+                # If password doesn't match display a message to the user
+                flash("Incorrect Username and/or Password")
+                redirect(url_for("login"))
+        else:
+            flash("Incorrect Username and/or Password")
+            redirect(url_for("login"))
+
+    return render_template("login.html")
 
 
 if __name__ == "__main__":
