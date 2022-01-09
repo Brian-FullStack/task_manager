7da18ffb2e3b0295bd90d2e1c66e6d072adf29d9
Brian-FullStack <brian.fullstack@gmail.com>
2022-01-09 16:58:05 +0000

    Add collapsible tasks setup

--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,10 @@
 /**Code from: Materialize - mobile collapse*/
+/** Side navigation*/
 $(document).ready(function(){
     $('.sidenav').sidenav({edge: "right"});
   });
+
+/** Collapsible tasks*/
+$(document).ready(function(){
+  $('.collapsible').collapsible();
+});
